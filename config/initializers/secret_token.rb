17d618e00c71e6549cd555d5c53a4f76dede0fde
Tanner Smith <tanner@tssoftware.net>
2011-08-25 16:37:46 -0400

    Add basic rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClassResponse::Application.config.secret_token = 'afa1496ce0b5311fd49f6c12ba24876ac7893bd9ed4a7f54bed1668ef13dd07d1f067dafc5495951944381124fafc0560f040c66dd7c122356e54b110ffaacd8'
