86f4ef5f0cb8054be616c61d899645d36c000b49
Tanner Smith <tanner@tssoftware.net>
2011-08-29 00:06:46 -0400

    Fix relationship for correct_answer.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :quiz
   has_many :answers, :class_name => "Answer"
-  has_one :correct_answer, :class_name => "Answer"
+  belongs_to :correct_answer, :class_name => "Answer"
 
   validates_presence_of :question, :quiz_id
 end
