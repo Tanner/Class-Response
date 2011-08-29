5364a12b6d1e06ccb1338d1e6612afa80069fcfb
Tanner Smith <tanner@tssoftware.net>
2011-08-29 10:17:52 -0400

    Fix broken quiz_session relationship for Submission.

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -3,6 +3,7 @@ class Submission < ActiveRecord::Base
   belongs_to :question
   belongs_to :answer
   belongs_to :quiz
+  belongs_to :quiz_session
 
   validates_presence_of :student_id, :question_id, :answer_id
 end
