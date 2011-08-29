3f7770c2429ff7550c49f2f9f8c50231f3e16855
Tanner Smith <tanner@tssoftware.net>
2011-08-29 10:05:36 -0400

    Remove :submissions table migrations.

--- db/migrate/20110827223943_add_quiz_to_submission.rb
+++
@@ -1,9 +0,0 @@
-class AddQuizToSubmission < ActiveRecord::Migration
-  def self.up
-    add_column :submissions, :quiz_id, :integer
-  end
-
-  def self.down
-    remove_column :submissions, :quiz_id
-  end
-end
