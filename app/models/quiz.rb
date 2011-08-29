ca5064778a72ae94475cecfb7a943a67930c5cc5
Tanner Smith <tanner@tssoftware.net>
2011-08-28 23:37:40 -0400

    Fix relationship for the current_question in Quiz

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,6 +1,6 @@
 class Quiz < ActiveRecord::Base
     has_many :questions
-    has_one :current_question, :class_name => "Question"
+    belongs_to :current_question, :class_name => "Question"
 
     validates_presence_of :name
 end
