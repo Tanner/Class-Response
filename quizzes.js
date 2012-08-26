ce73998d861abba26c0e27fe3a87ee058de1504b
Ryan Ashcraft <ryanashcraft@gmail.com>
2012-08-26 16:40:21 -0400

    Show the quiz id on quiz pages.

--- quizzes.js
+++ quizzes.js
@@ -2,14 +2,17 @@
 function show(req, res) {
 	var id = req.params.id;
 	
-	res.render('default_layout', function(err, html) {
-		if (err) {
-			console.log(err);
-			return;
-		}
+	res.render('default_layout', 
+		{ id: id },
+		function(err, html) {
+			if (err) {
+				console.log(err);
+				return;
+			}
 
-		res.send(html);
-	});
+			res.send(html);
+		}
+	);
 }
 
 exports.show = show;
