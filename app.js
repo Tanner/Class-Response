2b0df41a7cab4fc395cbaaffd9582d77e0f7a382
Ryan Ashcraft <ryanashcraft@gmail.com>
2012-08-26 16:30:55 -0400

    Create views and public directories. Use ejs to render welcome view.

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var app = express();
 var welcome = require('./welcome');
 var quizzes = require('./quizzes');
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
 // ClassResponse::Application.routes.draw do
 //   constraints(:id => /[0-9]+/) do
 //     match ':id' => 'quizzes#show'
--- welcome.js
+++ welcome.js
@@ -1,6 +1,9 @@
 
 function show(req, res) {
-	res.send('hello there');
+	// res.send('hello there');
+	res.render('welcome', function(err, html) {
+		res.send(html);
+	});
 }
 
 exports.show = show;
