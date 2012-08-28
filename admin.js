81735c85abc733b6b4b33afc9c77b1e0069af0f7
Tanner Smith <tanner@tssoftware.net>
2012-08-28 00:07:46 -0400

    Restructure code to show quiz id and name on admin page.
    Create functions to get all the quizzes and the data for a specific
    quiz.

--- admin.js
+++ admin.js
@@ -9,18 +9,54 @@ function index(req, res) {
 	});
 
 	client.on("connect", function(error) {
-		client.lrange("quizzes", 0, -1, function(err, reply) {	
-			res.render('admin', { quizzes: reply }, function(err, html) {
+		getQuizzes(client, function(quizzes) {
+			data = {quizzes: quizzes};
+
+			res.render('admin', data, function(err, html) {
 				if (err) {
 					console.log(err);
 					return;
 				}
 
 				res.send(html);
-			});
 
-			client.quit();
+				client.quit();
+			});
 		});
+	});
+}
+
+function getQuizzes(client, callback) {
+	client.lrange("quizzes", 0, -1, function(err, reply) {
+		quizzes = [];
+
+		if (reply) {
+			reply.forEach(function(id) {
+				getQuiz(client, id, function(quiz) {
+					quizzes.push(quiz);
+
+					if (quizzes.length == reply.length) {
+						return callback(quizzes);
+					}
+				});
+			});
+		}
+	});
+}
+
+function getQuiz(client, id, callback) {
+	var quiz = {};
+
+	quiz.id = id;
+
+	client.hget("quiz:" + id, "name", function(err, reply) {
+		if (err) {
+			return err;
+		}
+
+		quiz.name = reply;
+
+		return callback(quiz);
 	});
 }
 
